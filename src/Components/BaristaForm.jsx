9170a20971b51399e7cfbe103a8ee3d3831c9585
llynoirx <kathyho04@gmail.com>
2024-10-06 13:14:58 -0400

    checkpoint 1

--- src/Components/BaristaForm.jsx
+++ src/Components/BaristaForm.jsx
@@ -1,12 +1,32 @@
-
-
+import React, {Component, useState} from "react";
 
 function BaristaForm() {
+
+    const onNewDrink = () => {
+
+    }
+
+    const onCheckAnswer = () => {
+
+    }
   
   return (
-    <>
-    Hello
-    </>
+    <div>
+        <h2>Hi, I'd like to order a:</h2>
+        <form>
+
+        </form>
+        <button className="button submit" 
+                type="submit" 
+                onClick={onCheckAnswer}> 
+            Check Answer
+        </button>
+        <button className="button submit" 
+                type="new-drink-button" 
+                onClick={onNewDrink}> 
+            New Drink
+        </button>
+    </div>
   )
 }
 
